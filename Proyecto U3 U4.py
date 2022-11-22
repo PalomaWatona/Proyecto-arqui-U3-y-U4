85e0e708b0bc0c1930f0fa5b5701ab10871fc406
Martín <mvocilka20@alumnos.utalca.cl>
2022-11-21 21:14:43 -0300

    Se leen las instrucciones en el archivo "Instrucciones.txt"

--- Proyecto U3 U4.py
+++ Proyecto U3 U4.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+#Se lee el archivo de texto palabra por palabra
+with open('Instrucciones.txt', 'r') as archivo:
+    for linea in archivo:
+        for palabra in linea.split():
+            print(palabra)
+        print("")
+
